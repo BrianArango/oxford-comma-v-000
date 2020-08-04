9b04004b8a90a62a5a885eb45c7f873bcc3f54c7
Brian Arango <brianvictus@gmail.com>
2020-08-04 21:54:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,43 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+<<<<<<< HEAD
 def oxford_comma(array)
   if array.length == 1
     return "#{array[0]}"
@@ -8,3 +48,23 @@ def oxford_comma(array)
     return array.join(", ")
   end
 end
+=======
+<<<<<<< HEAD
+def oxford_comma(array)
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
+  end
+=======
+def oxford_comma(array)
+  "kiwi"
+  "kiwi"
+  
+end
+>>>>>>> bdc78000b769d28c0aa913b8faa4da1c76a39fce
+>>>>>>> db05636a1671a4bfa9b8e281c8baa180cec1f7e3
+=end
